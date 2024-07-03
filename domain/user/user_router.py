52d492670d146123878d30d3f753faa93e04e8fa
KimHaejoong1 <hjkim2124@gmail.com>
2024-07-03 17:28:29 +0900

    .env 파일을 사용하도록 수정

--- domain/user/user_router.py
+++ domain/user/user_router.py
@@ -6,13 +6,15 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 from starlette import status
+from starlette.config import Config
 
 from database import get_db
 from domain.user import user_crud, user_schema
 from domain.user.user_crud import pwd_context
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 60*24
-SECRET_KEY = "2c46ee7e6bdc6907e864e6bb3eeb7ef6f8e50f4b327cf50380d20041a67b6df8"
+config = Config('.env')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(config('ACCESS_TOKEN_EXPIRE_MINUTES'))
+SECRET_KEY = config('SECRET_KEY')
 ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/user/login")
 
